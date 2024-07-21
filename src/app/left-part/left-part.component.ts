23525d30b200adc9a69c5f9f35327be14126ac09
KrishnanBCG <149569182+KrishnanBCG@users.noreply.github.com>
2024-07-21 16:13:09 +0530

    Add files via upload

---
+++ src/app/left-part/left-part.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-part',
+  templateUrl: './left-part.component.html',
+  styleUrl: './left-part.component.css'
+})
+export class LeftPartComponent {
+
+file :string = "https://file-examples.com/storage/feedf97377669a777a26bd6/2017/10/file-sample_150kB.pdf"
+}
