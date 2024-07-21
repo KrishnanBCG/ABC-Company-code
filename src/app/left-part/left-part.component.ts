4cceb95bc1a6043a36fcd3c317de0dad323ba745
KrishnanBCG <149569182+KrishnanBCG@users.noreply.github.com>
2024-07-21 17:11:35 +0530

    Add files via upload

---
+++ src/app/left-part/left-part.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-part',
+  templateUrl: './left-part.component.html',
+  styleUrl: './left-part.component.css'
+})
+export class LeftPartComponent {
+
+file :string = "https://file-examples.com/storage/feedf97377669a777a26bd6/2017/10/file-sample_150kB.pdf"
+
+
+companyName :string = "ABC International Corporation Pvt …"
+
+city :string ="Thiruvananthapuram"
+
+currency : number =7122000.00
+
+location : string = "2E, 2nd Floor, VM Complex, Co-opertaive colony road, Alwarpet, Chennai 600018 . Tamilnadu."
+number : number = 9600900092
+gmail : string = "abcintl@gmail.com"
+GSTNO : string = "33AABCA2525D1Z7"
+date : string = "25 Mar 2024"
+RR : string = "Once you’ve received partial or full payment from your customer for this invoice, you can proceed to record it."
+R : string ="If you haven’t received partial or full payment from your customer for this invoice, please send a reminder for payment."
+}
